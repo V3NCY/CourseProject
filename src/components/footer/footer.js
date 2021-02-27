024e0e8af3c54cdebd656252d23f284036e86359
V3NCY <73014895+V3NCY@users.noreply.github.com>
2021-02-27 22:09:10 +0200

    modified footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -24,42 +24,84 @@ const Footer = () => {
               />
             </div>
             <div className="share my-3">
-              <FaInstagram size={38} className="ml-4" />
+              <a href="https://www.instagram.com">
+                <FaInstagram size={38} className="ml-4" />
+              </a>
 
-              <FaFacebook size={38} className="ml-4" />
+              <a href="https://www.facebook.com">
+                <FaFacebook size={38} className="ml-4" />
+              </a>
 
-              <FaTwitter size={38} className="ml-4" />
+              <a href="https://www.twitter.com">
+                <FaTwitter size={38} className="ml-4" />
+              </a>
             </div>
           </div>
-            <div className="col-md-2">
-              <h5 className="title">Партньори:</h5>
-              <ul>
-                <li className="list-unstyled">
-                  <a href="https://reactstrap.github.io/">Reactstrap</a>
-                </li>
-                <li className="list-unstyled">
-                  <a href="https://react-bootstrap.github.io/">
-                    React-Bootstrap
-                  </a>
-                </li>
-                <li className="list-unstyled">
-                  <a href="https://www.svgrepo.com/">SVGRepo</a>
-                </li>
-                <li className="list-unstyled">
-                  <a href="https://material-ui.com/">Material-UI</a>
-                </li>
-                <li className="list-unstyled">
-                  <a href="https://fsymbols.com/signs/stars/">FSymbols Stars</a>
-                </li>
-              </ul>
-            </div>
+          <div className="col">
+            <h5>За нас:</h5>
+            <ul>
+              <li className="list-unstyled">
+                <a href="https://europa.eu/european-union/abouteuropa/privacy-policy_bg">
+                  Политика за поверителност
+                </a>
+              </li>
+              <li className="list-unstyled">
+                <a href="https://iisda.government.bg/legal_information">
+                  Правна информация
+                </a>
+              </li>
+              <li className="list-unstyled">
+                <a href="http://www.meteo.bg/">Времето</a>
+              </li>
+            </ul>
           </div>
-          <div className="footer-copyright text-center py-3 col-12">
-            &copy; {new Date().getFullYear()} Copyright: По идея на официалния
-            сайт на{" "}
-            <a href="https://koprivshtitsa-bg.com/bg/">Община Копривщица </a>
+          <div className="col">
+            <h5>Община</h5>
+            <ul>
+              <li className="list-unstyled">
+                <a href="https://koprivshtitsa-bg.com/bg/municipality/profil-na-kupuvacha/">
+                  Профил на купувача
+                </a>
+              </li>
+              <li className="list-unstyled">
+                <a href="https://koprivshtitsa-bg.com/bg/municipality/administratsiya/">
+                  Администрация
+                </a>
+              </li>
+              <li className="list-unstyled">
+                <a href="https://koprivshtitsa-bg.com/bg/municipality/byudjeti-i-otcheti/">
+                  Бюджети и отчети
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-2">
+            <h5 className="title">Партньори:</h5>
+            <ul>
+              <li className="list-unstyled">
+                <a href="https://reactstrap.github.io/">Reactstrap</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="https://react-bootstrap.github.io/">React-Bootstrap</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="https://www.svgrepo.com/">SVGRepo</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="https://material-ui.com/">Material-UI</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="https://fsymbols.com/signs/stars/">FSymbols Stars</a>
+              </li>
+            </ul>
           </div>
         </div>
+        <div className="footer-copyright text-center py-3 col-12">
+          &copy; {new Date().getFullYear()} Copyright: По идея на официалния
+          сайт на{" "}
+          <a href="https://koprivshtitsa-bg.com/bg/">Община Копривщица </a>
+        </div>
+      </div>
     </footer>
   );
 };
